d7c6d29e3d95c42bd429607c6fa563b83bb6fa86
birdslisten <justin.schultz37@gmail.com>
2023-12-19 22:02:55 -0800

    added typeDefs based on the User and Book Model, also added starting point for resolvers

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,45 @@
+const typeDefs = `
+ type User {
+    _id: !ID
+    username: String!
+    email: String!
+    password: String!
+    SavedBookd: [Book!]!
+    bookCount: Int
+ }
+
+ type Book {
+   authors: [String]
+   description: String!
+   bookId: String!
+   image: String
+   link: String
+   title: String!
+ }
+
+ input BookInput {
+   authors: [String]
+   description: String!
+   bookId: String!
+   image: String
+   link: String
+   title: String!
+ }
+
+ type Query {
+
+ }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): User
+    login(email: String!, password: String!): Auth
+    saveBook(bookInput: BookInput!): User
+    removeBook(bookId: String!): User
+  }
+
+  type Auth {
+   token: ID!
+   user: User
+ }
+`;
+module.exports = typeDefs;
