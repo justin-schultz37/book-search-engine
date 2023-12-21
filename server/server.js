6fe27cc12e11e89d2f8dc54185b53417ef38030c
birdslisten <justin.schultz37@gmail.com>
2023-12-20 19:24:24 -0800

    updated server and auth to fix token issues

--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
+const { ApolloServer } = require('@apollo/server')
+const { expressMiddleware } = require('@apollo/server/express4');;
 const path = require('path');
-const db = require('./config/connection');
+const { authMiddleware } = require('./utils/auth');
+
 const { typeDefs, resolvers } = require('./schemas');
-const routes = require('./routes');
+const db = require('./config/connection');
 
-const app = express();
+
 const PORT = process.env.PORT || 3001;
-
+const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -16,17 +18,13 @@ const server = new ApolloServer({
 async function startServer() {
   await server.start();
 
-  // Apply Apollo middleware to Express
-  server.applyMiddleware({ app });
-
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
 
-  // if (process.env.NODE_ENV === 'production') {
-  //   app.use(express.static(path.join(__dirname, '../client/build')));
-  // }
+  app.use('/graphql', expressMiddleware(server, {
+    context: authMiddleware
+  }));
 
-  // if we're in production, serve client/build as static assets
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
@@ -34,8 +32,6 @@ async function startServer() {
       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     });
   }
-
-  app.use(routes);
 
   db.once('open', () => {
     app.listen(PORT, () => {
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require('apollo-server-express');
+const { GraphQLError } = require('graphql');
 const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
@@ -6,31 +6,33 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
+  AuthenticationError: new GraphQLError('Could not authenticate user.', {
+    extensions: {
+      code: 'UNAUTHENTICATED',
+    },
+  }),
   authMiddleware: function ({ req }) {
-    // allows token to be sent via req.query or headers
-    let token = req.headers.authorization || '';
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
-    // ["Bearer", "<tokenvalue>"]
-    if (token.startsWith('Bearer ')) {
-      token = token.slice(7);
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
     }
 
     if (!token) {
-      throw new AuthenticationError('You have no token!');
+      return req;
     }
 
-    // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      return { user: data };
-    } catch (error) {
-      console.error('Invalid token:', error);
-      throw new AuthenticationError('Invalid token');
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
     }
-  },
 
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
+    return req;
+  },
+  signToken: function ({ email, username, _id }) {
+    const payload = { email, username, _id };
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
