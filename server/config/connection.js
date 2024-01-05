21c2ba5021eba5071c84becad3c656afba5278d8
birdslisten <justin.schultz37@gmail.com>
2024-01-05 11:53:45 -0800

    added updated database access for heroku deployment

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,37 @@
-const mongoose = require('mongoose');
-require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+const uri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks';
 
-module.exports = mongoose.connection;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+async function connect() {
+    try {
+        // Connect the client to the server (optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
+}
+
+async function close() {
+    try {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+        console.log("MongoDB connection closed.");
+    } catch (error) {
+        console.error("Error closing MongoDB connection:", error);
+    }
+}
+
+module.exports = { connect, close };
